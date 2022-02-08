2b44a5d8700f095ebb9a03cea9314d60444833cf
Marc Grabanski <marc@mjg.in>
2022-02-08 14:37:42 -0600

    Upgrade packages, add link back button

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -75,6 +75,7 @@ const TemplateWrapper = (props) => {
               {!frontmatter ? null : (
                 <h2>{`${frontmatter.section} – ${frontmatter.title}`}</h2>
               )}
+              <h2 class="button"><a href="https://frontendmasters.com/courses/realtime/">Complete Intro to Realtime Videos&nbsp;▶️&nbsp;</a></h2>
             </div>
             <div className="content-container">
               <div className="main">{props.children}</div>
